294bb32ed2d572d63655cecfdcce4ea6411f2e21
artemgon <artemhoncharuk123@gmail.com>
2024-12-31 23:07:57 +0200

    Task 1 is done.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@
     {
         static void Main()
         {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("It's easy to win forgivness for being wrong;");
+            Console.WriteLine("Being right is what gets you into real trouble.");
+            Console.WriteLine("Bjarne Stroustrup");
+            Console.ResetColor();
         }
     }
 }
