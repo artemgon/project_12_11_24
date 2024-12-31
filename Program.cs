77ecbacbba9e178fa46451c65f6f86bb0471b43d
artemgon <artemhoncharuk123@gmail.com>
2024-12-31 23:21:56 +0200

    Task 3 is created.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,22 @@
     {
         static void Main()
         {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Enter a number: ");
+            int number = int.Parse(Console.ReadLine() ?? "");
+            Console.WriteLine($"Reverse of {number} is {ReverseNumber(number)}");
+        }
+
+        static int ReverseNumber(int number)
+        {
+            int reversed = 0;
+            while (number != 0)
+            {
+                int digit = number % 10;
+                reversed = reversed * 10 + digit;
+                number /= 10;
+            }
+            return reversed;
         }
     }
 }
