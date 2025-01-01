871e8e6b2583521c6cce5c104222ac55ebfcbf9b
artemgon <artemhoncharuk123@gmail.com>
2025-01-01 07:08:21 +0200

    Task 5 is done.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,37 @@
     {
         static void Main()
         {
+            try
+            {
+                Console.WriteLine("Enter two positive numbers: ");
+                int A = PositiveNumber(), B = PositiveNumber();
+                for (int i = A; i <= B; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write(i + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+            }
+        }
+        static int PositiveNumber()
+        {
+            int n = int.Parse(Console.ReadLine() ?? "");
+            if (n <= 0)
+            {
+                throw new ArgumentException("The number must be positive");
+            }
+            else
+                return n;
         }
     }
 }
