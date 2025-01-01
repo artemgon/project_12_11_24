ce41ecf3db99841365d7db39c3fca66e9e270369
artemgon <artemhoncharuk123@gmail.com>
2025-01-01 08:15:18 +0200

    Task 4 is done.

--- Program.cs
+++ Program.cs
@@ -6,21 +6,32 @@
         {
             try
             {
-                int a = positive(), b = positive(), c = positive(), f = positive();
-                int number = a * 1000 + b * 100 + c * 10 + f;
-                Console.WriteLine(number);
+                int number = validNumber();
+                Console.WriteLine("Enter two digit places: ");
+                int first = int.Parse(Console.ReadLine() ?? "");
+                int second = int.Parse(Console.ReadLine() ?? "");
+                if (first < 1 || first > 6 || second < 1 || second > 6)
+                {
+                    throw new Exception("Invalid digit places");
+                }
+                char[] digits = number.ToString().ToCharArray();
+                char temp = digits[first - 1];
+                digits[first - 1] = digits[second - 1];
+                digits[second - 1] = temp;
+                string newNumber = new string(digits);
+                Console.WriteLine($"Modified number: {newNumber}");
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine("Error... " + ex.Message);
             }
         }
-        static int positive()
+        static int validNumber()
         {
             int n = int.Parse(Console.ReadLine() ?? "");
-            if(n <= 0)
+            if(n < 100000 || n >= 1000000)
             {
-                throw new Exception("Number must be positive");
+                throw new Exception("Number isn't valid");
             }
             return n;
         }
