0e3e0dc2f550f0a90ad88f0c38e651729961d51a
artemgon <artemhoncharuk123@gmail.com>
2025-01-01 07:27:53 +0200

    Task 1 is done.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,39 @@
     {
         static void Main()
         {
+            try
+            {
+                int number = goodNumber();
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static int goodNumber()
+        {
+            int n = int.Parse(Console.ReadLine() ?? "");
+            if(n < 1 || n >100)
+            {
+                throw new Exception("Number is not in the range 1-100");
+            }
+            return n;
         }
     }
 }
