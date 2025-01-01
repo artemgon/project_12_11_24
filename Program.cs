3ebfc09c0e1cc23cd63eb342a4f60fae0c108aa0
artemgon <artemhoncharuk123@gmail.com>
2025-01-01 10:12:48 +0200

    Task 7 is done.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,36 @@
     {
         static void Main()
         {
+            try
+            {
+                Console.WriteLine("Enter two numbers: ");
+                Console.WriteLine("Start: ");
+                int start = int.Parse(Console.ReadLine() ?? "");
+                Console.WriteLine("End: ");
+                int end = int.Parse(Console.ReadLine() ?? "");
+                if (start > end)
+                {
+                    int temp = start;
+                    start = end;
+                    end = temp;
+                }
+                if (start == end)
+                {
+                    Console.WriteLine("No numbers");
+                    return;
+                }
+                for (int i = start; i <= end; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
